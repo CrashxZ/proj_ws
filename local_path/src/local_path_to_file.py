cb961b4a70a2923dc05564f6cf1e182bc2345972
Mainak <mainakme2140@gmail.com>
2019-09-03 18:06:20 +0700

    Json Save

--- local_path/src/local_path_to_file.py
+++ local_path/src/local_path_to_file.py
@@ -1,42 +1,48 @@
 #! /usr/bin/env python
 import rospy
 import numpy as np
+import json
 import matplotlib.pyplot as plt
-import importlib 
+import importlib
 from nav_msgs.msg import Odometry, Path
 from geometry_msgs.msg import Pose, PoseStamped, Point, PointStamped
 import time
- 
-class SavePath(object):
 
-    p1 = np.array([0,0,0])
-    p2 = np.array([0,0,0])
-    
+
+class SavePath(object):
+    p1 = np.array([0, 0, 0])
+    p2 = np.array([0, 0, 0])
+
     def __init__(self):
         self._point = PointStamped()
         self._currentPoint = PointStamped()
         self._point_sub = rospy.Subscriber('/dji_sdk/local_position', PointStamped, self.sub_callback)
         self.write_to_file()
 
-
-
-
- 
- 
     def sub_callback(self, msg):
+        self._point = [msg.point.x, msg.point.y, msg.point.z]
+        # rospy.loginfo(self._point)
 
-        self._point = [msg.point.x,msg.point.y,msg.point.z]
-        #rospy.loginfo(self._point)
+    def saveJSON(self, coordinate):
+        pointn = {
+            "x": coordinate[0],
+            "y": coordinate[1],
+            "z": coordinate[2]
+        }
+        temp = json.dumps(pointn)
+        rate = rospy.Rate(20)
+        with open("coordinates.json", "w") as file:
+            while not rospy.is_shutdown():
+                json.dump(temp, file)
+                file.write("\n")
+                rate.sleep()
 
 
-	
-    
     def write_to_file(self):
-        
         rospy.loginfo("Start to write all poses from RtabMap.")
-	    #rospy.loginfo(self._pose)
-        
-        #rospy.loginfo(self._currentPoint)
+        # rospy.loginfo(self._pose)
+
+        # rospy.loginfo(self._currentPoint)
         rate = rospy.Rate(20)
         with open('localPoseStamped.txt', 'w') as file:
             while not rospy.is_shutdown():
@@ -46,22 +52,7 @@ class SavePath(object):
         rospy.loginfo("Written all Poses to poses.txt file")
 
 
-
-	
-	
-
-
-	
-        
- 
- 
 if __name__ == "__main__":
-    rospy.init_node('spot_recorder', log_level=rospy.INFO) 
+    rospy.init_node('spot_recorder', log_level=rospy.INFO)
     save_spots_object = SavePath()
-    #rospy.spin() 
-
-
-
-    
-
-
+    # rospy.spin()
