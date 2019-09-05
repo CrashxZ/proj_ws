80fa900db51d35769f89fadddbeaa53ad2884a8c
CrashxZ <mainakme2140@gmail.com>
2019-09-05 10:24:57 +0000

    jetson

---
+++ local_path/src/local_path_to_file.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+import rospy
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import importlib
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import Pose, PoseStamped, Point, PointStamped
+import time
+class SavePath(object):
+
+    def __init__(self):
+        self._currentPoint = PointStamped()
+        self._coordinate = ""
+        self._point_sub = rospy.Subscriber('/dji_sdk/local_position', PointStamped, self.sub_callback)
+        self.saveJSON()
+
+    def sub_callback(self, msg):
+        self._coordinate = {
+            "x": msg.point.x,
+            "y": msg.point.y,
+            "z": msg.point.z
+            }
+        
+
+    def saveJSON(self):
+        rate = rospy.Rate(20)
+        rospy.loginfo("Start to write all points.")
+        with open("coordinates.json", "w") as file:
+            while not rospy.is_shutdown():
+                json.dump(self._coordinate, file)
+                file.write("\n")
+                rospy.loginfo(self._coordinate)
+                rate.sleep()
+
+    def loadJSON(self):
+        with open("coordinates.json", "r") as file:
+            pathData = json.load(file)
+
+
+if __name__ == "__main__":
+    rospy.init_node('spot_recorder', log_level=rospy.INFO)
+    save_spots_object = SavePath()
+    # rospy.spin()
