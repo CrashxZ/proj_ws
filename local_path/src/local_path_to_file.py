291aa7ab71632be82dff4d10a0e593bad10d65c3
Mainak Mondal <mainakme2140@gmail.com>
2019-09-05 17:03:02 +0700

    Update local_path_to_file.py

--- local_path/src/local_path_to_file.py
+++ local_path/src/local_path_to_file.py
@@ -10,50 +10,33 @@ import time
 class SavePath(object):
 
     def __init__(self):
-        self._point = PointStamped()
         self._currentPoint = PointStamped()
-        self.coordinate = ""
+        self._coordinate = ""
         self._point_sub = rospy.Subscriber('/dji_sdk/local_position', PointStamped, self.sub_callback)
-        #self.write_to_file()
         self.saveJSON()
 
     def sub_callback(self, msg):
-        self.coordinate = {
+        self._coordinate = {
             "x": msg.point.x,
             "y": msg.point.y,
             "z": msg.point.z
             }
         
-        rospy.loginfo(self.coordinate)
 
     def saveJSON(self):
         rate = rospy.Rate(20)
+        rospy.loginfo("Start to write all points.")
         with open("coordinates.json", "w") as file:
             while not rospy.is_shutdown():
-                json.dump(self.coordinate, file)
-                file.write("\n")
-                rospy.loginfo(self.coordinate)
+                if(self._coordinate != "")
+                    json.dump(self._coordinate, file)
+                    file.write("\n")
+                    rospy.loginfo(self._coordinate)
                 rate.sleep()
 
     def loadJSON(self):
         with open("coordinates.json", "r") as file:
             pathData = json.load(file)
-
-        # for coordinate in pathData:
-        #     print(coordinate)
-
-    def write_to_file(self):
-        rospy.loginfo("Start to write all poses from RtabMap.")
-        # rospy.loginfo(self._pose)
-
-        # rospy.loginfo(self._currentPoint)
-        rate = rospy.Rate(20)
-        with open('localPoseStamped.txt', 'w') as file:
-            while not rospy.is_shutdown():
-                file.write(str(self._point) + ' \n')
-                rospy.loginfo(self._point)
-                rate.sleep()
-        rospy.loginfo("Written all Poses to poses.txt file")
 
 
 if __name__ == "__main__":
