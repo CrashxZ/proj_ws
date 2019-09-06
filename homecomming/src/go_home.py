380f607d783e6912562d27601dadfd5da8c96fd2
Mainak <mainakme2140@gmail.com>
2019-09-06 15:07:41 +0700

    GoHome script

---
+++ homecomming/src/go_home.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+import rospy
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import importlib
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import Pose, PoseStamped, Point, PointStamped
+import time
+
+
+class GoHome(object):
+
+    def __init__(self):
+        self._currentPoint = PointStamped()
+        self._coordinate = ""
+        self._point_sub = rospy.Subscriber('/dji_sdk/local_position', PointStamped, self.sub_callback)
+        self.saveJSON()
+
+    def sub_callback(self, msg):
+        self._coordinate = {
+            "x": msg.point.x,
+            "y": msg.point.y,
+            "z": msg.point.z
+        }
+
+    def loadJSON(self):
+        with open("coordinates.json", "r") as file:
+            pathData = json.load(file)
+
+    def deltaQ(self):
+
+
+if __name__ == "__main__":
+    rospy.init_node('spot_recorder', log_level=rospy.INFO)
+    go_home_object = GoHome()
+    # rospy.spin()
