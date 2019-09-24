395b01f43f1b917e424fb3882480b6a0f7db526a
Mainak <mainakme2140@gmail.com>
2019-09-24 15:41:41 +0700

    Odom and Path from RtabMap

--- poses_to_file/src/odom_to_file.py
+++ poses_to_file/src/odom_to_file.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 import rospy
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from nav_msgs.msg import Odometry, Path
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped, Point
 import time
  
  
@@ -11,26 +12,33 @@ class SavePoses(object):
     def __init__(self):
         
         self._pose = Pose()
+	self._currentpose = ""
         self._pose_sub = rospy.Subscriber('/odom', Odometry , self.sub_callback)
         self.write_to_file()
  
     def sub_callback(self, msg):
         
         self._pose = msg.pose.pose
-    
+	self._currentpose = {"x": self._pose.position.x, 
+			     "y": self._pose.position.y, 
+			     "z": self._pose.position.z}
+	
+    	
+
     def write_to_file(self):
         
-        rospy.loginfo("Professor Dmitry, Hello!")
-        rospy.loginfo("Start to write all poses from RtabMap.")
+        rospy.loginfo("Odom data from RtabMap.")
+
         rate = rospy.Rate(5)
-	with open('poses.txt', 'w') as file:
-            
-            while self._pose:
+	with open("RtabMap_Odom.json", "w") as file:
+            while not rospy.is_shutdown():
+                if (self._currentpose != ""):
+			rospy.loginfo(self._currentpose)
+			json.dump(self._currentpose, file)
+			file.write("\n")
+            	rate.sleep()
                     
-		file.write(str(self._pose.position.x) + ' ' + str(self._pose.position.y) + ' ' + str(self._pose.position.z) + '\n')
-                rate.sleep()
-                    
-        rospy.loginfo("Written all Poses to poses.txt file")
+        rospy.loginfo("Written all Odom to poses.txt file")
         
  
  
--- poses_to_file/src/path_to_file.py
+++ poses_to_file/src/path_to_file.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import rospy
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from nav_msgs.msg import Odometry, Path
@@ -8,33 +9,20 @@ import time
  
 class SavePath(object):
 
-    p1 = np.array([0,0,0])
-    p2 = np.array([0,0,0])
     def __init__(self):
-        self._path = Path()
-        self._pose = PoseStamped()
+
 	self._currentposition =  ""
-	self._deltaQ = ""
         self._path_sub = rospy.Subscriber('/rtabmap/mapPath', Path , self.sub_callback)
         self.write_to_file()
-	#rospy.loginfo(type(self._pose))
-	#rospy.loginfo(type(self._path))
-	#rospy.loginfo(type(self._path_sub))
-
- 
  
     def sub_callback(self, msg):
 	
-	p1 = np.array([msg.poses[-1].pose.position.x, msg.poses[-1].pose.position.y, msg.poses[-1].pose.position.z])
-    	p2 = np.array([msg.poses[-2].pose.position.x, msg.poses[-2].pose.position.y, msg.poses[-2].pose.position.z])
-	deltaQ= p1-p2
-
 	self._temp = msg.poses[-1]
-	self._pose = str(self._temp.pose.position.x) + ' ' + str(self._temp.pose.position.y) + ' ' + str(self._temp.pose.position.z)
-	self._currentposition = np.array([self._temp.pose.position.x,self._temp.pose.position.y,self._temp.pose.position.z])
+	self._currentposition = {"x": self._temp.pose.position.x, 
+				 "y": self._temp.pose.position.y, 
+			         "z": self._temp.pose.position.z}
 
 	rospy.loginfo(self._currentposition)
-	rospy.loginfo(" Delta Q =" +str(deltaQ))
 	
 
 	
@@ -42,18 +30,16 @@ class SavePath(object):
     def write_to_file(self):
         
         rospy.loginfo("Start to write all poses from RtabMap.")
-	#rospy.loginfo(self._pose)
 	
-	
-
         rate = rospy.Rate(20)
-	with open('poses_from_path.txt', 'w') as file:
-            
-            while not rospy.is_shutdown():    
-		    file.write(str(self._currentposition) + " Delta Q =" + str(self._currentposition) + ' \n')
-                rate.sleep()
+	with open("RtabMap_Path.json", "w") as file:
+            while not rospy.is_shutdown():
+                if (self._currentposition != ""):
+			json.dump(self._currentposition, file)
+			file.write("\n")
+            	rate.sleep()
                     
-        rospy.loginfo("Written all Poses to poses.txt file")
+        rospy.loginfo("Written Path to RtabMap_Path.json file")
 	
         
  
