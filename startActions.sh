e98f7b05718b0fa526d9f303e106df9032398524
Mainak Mondal <mainakme2140@gmail.com>
2019-10-17 18:21:52 +0700

    Update startActions.sh

--- startActions.sh
+++ startActions.sh
@@ -2,17 +2,24 @@
 
 echo "Starting Services!"
 
-cd /dji_sdk
+
+
+
+sudo usermod -a -G dialout nvidia
+
+
+gnome-terminal -- bash -c "sleep 10;cd $HOME/dji_sdk;source devel/setup.bash;rosservice call /dji_sdk/activation {};rosservice call /dji_sdk/set_local_pos_ref {};exec bash"
+
+
+gnome-terminal -- bash -c "sleep 11;cd $HOME/proj_ws;source devel/setup.bash;rosrun local_path local_path_to_file.py;exec bash"
+
+gnome-terminal -- bash -c "sleep 11;cd $HOME/proj_ws;source devel/setup.bash;rosrun rtabmap_to_file all_in_one.py;exec bash"
+
+gnome-terminal -- bash -c "sleep 11;cd $HOME/proj_ws;source devel/setup.bash;rosrun zed_to_file one_for_all.py;exec bash"
+
+cd $HOME/dji_sdk
 source devel/setup.bash
 roslaunch dji_sdk sdk.launch
 
-gnome-terminal -e "bash -c cd /dji_sdk;source devel/setup.bash;rosservice call /dji_sdk/activation {};rosservice call /dji_sdk/set_local_pos_ref {};bash"
-echo "Drone activated and local position set!"
-
-gnome-terminal -e "bash -c cd /proj_ws;source devel/setup.bash;roslaunch local_path local_path_to_file.py.py;bash"
-echo "Dji SDK Local position recording!"
-gnome-terminal -e "bash -c cd /proj_ws;source devel/setup.bash;roslaunch rtabmap_to_file all_in_one.py;bash"
-
-gnome-terminal -e "bash -c cd /proj_ws;source devel/setup.bash;roslaunch zed_to_file one_for_all.py;bash"
 
 $SHELL
